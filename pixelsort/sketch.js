41c332059ec07528632ef1335474ff54dccb5dd8
jsc-264 <joestephen264@gmail.com>
2026-09-16 16:15:14 +0100

    pixelsort: added pic of chicago to assets

--- pixelsort/sketch.js
+++ pixelsort/sketch.js
@@ -1,7 +1,13 @@
-function setup() {
-    createCanvas(800, 600);
+const DIM = 10
+let chi
+
+async function setup() {
+    chi = await loadImage("./assets/chicago.jpg")
+    createCanvas(500, 500);
 }
 
 function draw() {
     background(220);
+
+    image(chi, 0, 0, width, height)
 }
