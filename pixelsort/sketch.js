ff19530130ae42f49bc77913f4b5b3d33da98ef3
jsc-264 <joestephen264@gmail.com>
2026-09-16 17:20:20 +0100

    pixelsort: made a grid

--- pixelsort/sketch.js
+++ pixelsort/sketch.js
@@ -1,8 +1,19 @@
-async function setup() {
-    createCanvas(500, 500);
+const DIM = 10
+let w
 
+function setup() {
+    createCanvas(500, 500);
+    w = width / DIM
 }
 
 function draw() {
-    background(50)
+    background(50);
+
+    for (let j = 0; j < DIM; j++) {
+        for (let i = 0; i < DIM; i++) {
+            const x = i * w
+            const y = j * w
+            rect(x, y, w)
+        }
+    }
 }
