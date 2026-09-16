1eae3f8cbae2203ecbc167ce4a57e2c9d2895f6f
jsc-264 <joestephen264@gmail.com>
2026-09-16 17:19:02 +0100

    pixelsort: starting over

--- pixelsort/sketch.js
+++ pixelsort/sketch.js
@@ -1,26 +1,8 @@
-let chi;
-
 async function setup() {
     createCanvas(500, 500);
-    chi = await loadImage("./assets/chicago.jpg");
-    chi.resize(50, 50);
-    noSmooth()
+
 }
 
 function draw() {
-    let newChi = sortPixels(chi)
-    image(newChi, 0, 0, width, height);
-    noLoop()
-}
-
-function sortPixels(img){
-    let newImg = createImage(img.width, img.height)
-    newImg.loadPixels()
-    for (let x = 0; x < img.width; x++) {
-        for (let y = 0; y < img.height; y++) {
-            newImg.set(x, y, img.get(x, y))
-        }
-    }
-    newImg.updatePixels()
-    return newImg
+    background(50)
 }
