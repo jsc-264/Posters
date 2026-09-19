0b68e1c132ddb0be655b48c4107babc5f6439f6e
jsc-264 <joestephen264@gmail.com>
2026-09-19 16:19:48 +0100

    zach: added colour

--- zach/sketch.js
+++ zach/sketch.js
@@ -1,29 +1,39 @@
 function blob(x, y, r) {
     beginShape()
-    for (let a = 0; a < 360; a++) {
+    for (let a = 0; a < 360; a+=18) {
+        // we cant use translate here cause noise values must be positive
         const nx = (r * cos(a) + x) / 200
         const ny = (r * sin(a) + y) / 200
         const nVal = noise(nx, ny)
 
-        const change = map(nVal, 0, 1, -50, 50)
-        const pRad = r + change
+        // calculating offset and adding it to radius
+        const off = map(nVal, 0, 1, -r, r)
+        const newR = r + off
 
-        const vx = pRad * cos(a) + x
-        const vy = pRad * sin(a) + y
+        // polar to cartesian coords
+        const vx = newR * cos(a) + x
+        const vy = newR * sin(a) + y
 
         vertex(vx, vy)
     }
-    endShape()
+    endShape(CLOSE)
 }
 
+const scl = 1.3
+
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(400*scl, 500*scl);
     angleMode(DEGREES)
 }
 
-function draw() {
-    background(220);
+function draw(){
+    background("#00272B");
 
-    blob(width/2, height/2, 100)
-    noLoop()
+    noFill()
+    stroke("#e0ff4f0f")
+    strokeWeight(5)
+    for (let i = 0; i < 500; i++) {
+        const r = i * 1.5
+        blob(width / 3, height / 4, r)
+    }
 }
