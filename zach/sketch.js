ec63a728dc75441ccbe7706d8715d5f497ca5b4d
jsc-264 <joestephen264@gmail.com>
2026-09-19 15:26:22 +0100

    zach: made blob function

--- zach/sketch.js
+++ zach/sketch.js
@@ -1,7 +1,24 @@
+function blob(x, y, r) {
+    push()
+    translate(x, y)
+    beginShape()
+    for (let a = 0; a < 360; a++) {
+        const vx = (r + random(1)) * cos(a)
+        const vy = (r + random(1)) * sin(a)
+        vertex(vx, vy)
+    }
+    endShape(CLOSE)
+    pop()
+}
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(500, 500);
+    angleMode(DEGREES)
 }
 
 function draw() {
     background(220);
+
+    blob(width/2, height/2, 200)
+    noLoop()
 }
