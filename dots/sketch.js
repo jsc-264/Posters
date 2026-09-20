40b139fefc974b1d072e33d610611a8ced656ec1
jsc-264 <joestephen264@gmail.com>
2026-09-20 22:16:28 +0100

    dots: distorted dots by noise

--- dots/sketch.js
+++ dots/sketch.js
@@ -1,5 +1,5 @@
-const DIMX = 15
-const DIMY = 20
+const DIMX = 150
+const DIMY = 200
 
 let w
 let h
@@ -16,11 +16,20 @@ function draw() {
 
     for (let j = 0; j < DIMY; j++) {
         for (let i = 0; i < DIMX; i++) {
-            const x = w * i + w/2
-            const y = h * j + h/2
+            let x = w * i + w / 2
+            let y = h * j + h / 2
 
-            strokeWeight(5)
+            let n = noise(x / 100, y / 100)
+
+            const offX = map(n, 0, 1, -w * 10, w * 10)
+            const offY = map(n, 0, 1, -h * 10, h * 10)
+
+            x += offX
+            y += offY
+
+            strokeWeight(1)
             point(x, y)
         }
     }
+    noLoop()
 }
