6b5161259256f9b3d978779f727be8979bd0c72a
jsc-264 <joestephen264@gmail.com>
2026-09-20 22:25:56 +0100

    dots: turned dots into beginshapes

--- dots/sketch.js
+++ dots/sketch.js
@@ -1,5 +1,5 @@
-const DIMX = 150
-const DIMY = 200
+const res = 150
+const rows = 500
 
 let w
 let h
@@ -7,29 +7,31 @@ let h
 function setup() {
     createCanvas(400, 600);
     textAlign(CENTER, CENTER)
-    w = width / DIMX
-    h = height / DIMY
+    w = width / res
+    h = height / rows
 }
 
 function draw() {
     background(220);
 
-    for (let j = 0; j < DIMY; j++) {
-        for (let i = 0; i < DIMX; i++) {
+    for (let j = 0; j < rows; j++) {
+        // noFill()
+        beginShape()
+        for (let i = 0; i < res; i++) {
             let x = w * i + w / 2
             let y = h * j + h / 2
 
             let n = noise(x / 100, y / 100)
 
-            const offX = map(n, 0, 1, -w * 10, w * 10)
-            const offY = map(n, 0, 1, -h * 10, h * 10)
+            const offX = map(n, 0, 1, -w*10, w*10)
+            const offY = map(n, 0, 1, -w*10, w*10)
 
             x += offX
             y += offY
 
-            strokeWeight(1)
-            point(x, y)
+            vertex(x, y)
         }
+        endShape()
     }
     noLoop()
 }
