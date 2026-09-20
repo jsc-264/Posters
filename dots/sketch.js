19858fb4cccfc10f0f0472f9eb17be5936ca95f9
jsc-264 <joestephen264@gmail.com>
2026-09-20 22:12:10 +0100

    dots: made grid of dots

--- dots/sketch.js
+++ dots/sketch.js
@@ -1,7 +1,26 @@
+const DIMX = 15
+const DIMY = 20
+
+let w
+let h
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(400, 600);
+    textAlign(CENTER, CENTER)
+    w = width / DIMX
+    h = height / DIMY
 }
 
 function draw() {
     background(220);
+
+    for (let j = 0; j < DIMY; j++) {
+        for (let i = 0; i < DIMX; i++) {
+            const x = w * i + w/2
+            const y = h * j + h/2
+
+            strokeWeight(5)
+            point(x, y)
+        }
+    }
 }
